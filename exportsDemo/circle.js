a8e0228110011762da4ba59a041a6b1893a3211e
DurgaPrasad <durga.pras10@gmail.com>
2014-08-29 06:11:01 +0530

    exports demo

---
+++ exportsDemo/circle.js
@@ -0,0 +1,9 @@
+var util = require('./utility');
+var radius=10;
+
+console.log(util.circleArea(radius));
+console.log(util.circlePerimeter(radius));
+
+//the following function can't be used as it is not exported from the utility module
+//console.log(getPiValue());
+
---
+++ exportsDemo/utility.js
@@ -0,0 +1,14 @@
+var pi=3.18
+
+function getPiValue(){
+return pi;
+}
+
+exports.circleArea=function getCircleArea(radius){
+return pi*radius*radius;
+}
+
+
+exports.circlePerimeter=function getPerimeter(radius){
+return 2*pi*radius;
+}
